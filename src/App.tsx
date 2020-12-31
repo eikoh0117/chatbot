c40ab00234f350c5d41a872372bc80c691c0f2e2
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-31 20:16:01 +0900

    Define type of State

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import defaultDataset from "./dataset";
 import "./assets/styles/style.scss";
 
-export default class App extends React.Component {
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
+type State = {
+  answers: [];
+  chats: [];
+  currentId: string;
+  dataset: unknown;
+  open: boolean;
+};
+export default class App extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -15,6 +24,10 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <div></div>;
+    return (
+      <section className="c-section">
+        <div className="c-box">{this.state.currentId}</div>
+      </section>
+    );
   }
 }

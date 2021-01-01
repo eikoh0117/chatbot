4fc67e6fef1808950eb3c7fbb6e2329c7c202fda
Hidemitsu Aoki <olympos0117@gmail.com>
2021-01-01 18:56:27 +0900

    Modify haw to export

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,32 @@
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// admin.initializeApp();
+// const db = admin.firestore();
+
+// const sendResponse = (
+//   response: functions.Response,
+//   statusCode: number,
+//   body: any
+// ) => {
+//   response.send({
+//     statusCode,
+//     body: JSON.stringify(body),
+//   });
+// };
+
+// exports.addDataset = functions.https.onRequest(async (req: any, res: any) => {
+//   if (req.method !== "POST") {
+//     sendResponse(res, 405, { error: "Invalid Request!" });
+//   } else {
+//     const dataset = req.body;
+//     for (const key of Object.keys(dataset)) {
+//       const data = dataset[key];
+//       await db.collection("questions").doc(key).set(data);
+//     }
+//     sendResponse(res, 200, { message: "Successfully added dataset!" });
+//   }
+// });
+
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
@@ -14,17 +43,24 @@ const sendResponse = (
   });
 };
 
+/**
+ * Execute the following command in your Terminal app
+ * curl -X POST https://YOUR_REGION-YOUR_PROJECT_NAME.cloudfunctions.net/addDataset -H "Content-Type:application/json" -d @dataset.json
+ */
+
 export const addDataset = functions.https.onRequest(
   async (req: any, res: any) => {
     if (req.method !== "POST") {
-      sendResponse(res, 405, { error: "Invalid Request!" });
+      sendResponse(res, 405, { error: "Invalid Request" });
     } else {
       const dataset = req.body;
       for (const key of Object.keys(dataset)) {
         const data = dataset[key];
         await db.collection("questions").doc(key).set(data);
       }
-      sendResponse(res, 200, { message: "Successfully added dataset!" });
+      sendResponse(res, 200, {
+        message: "Successfully added dataset! WooHoo!",
+      });
     }
   }
 );
